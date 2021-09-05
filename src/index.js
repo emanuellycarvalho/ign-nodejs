615ac1d0b79ee364be25b9f020939b6f16fc0f30
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-09-05 16:52:40 -0300

    Autoreload with nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({ message: "Hello world, but with json"});
+    return response.json({ message: "Hello world, but with json and nodemon" });
 });
 
 //startar a aplicação na porta  do parâmetro

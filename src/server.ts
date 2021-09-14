80edea379ff0e208acd7a8b421d1b46f69bd656d
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-09-14 20:47:46 -0300

    Improving autoload with flags

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/", function(request, response){
-    return response.json({ message: "Hello world" });
+    return response.json({ message: "Autoreload worked" });
 });
 
 app.listen(3333, () => console.log("Server is running!"));

5c1741ff1e73f52e0df8ef7756ee6250de1e2133
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-09-14 23:06:06 -0300

    Creating caterogy services (applying SOLID)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { request, response, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/category/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -7,11 +8,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    if(categoriesRepository.findByName(name)){
-        return response.status(400).json({ error: "Category already exists!" })
-    }
-
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({ name, description });
+    
     return response.status(201).json({ message: "Category created!" });
 });
 

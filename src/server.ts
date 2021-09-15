9c7cb38f8e60ac9ad0f7a203830fc149348b7824
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-09-14 21:08:00 -0300

    Debugger config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get("/", function(request, response){
     return response.json({ message: "Autoreload worked" });
+});
+
+app.post("/course", (request, response) => {
+    // console.log(request.body);    [usaria p debugar]
+    const { name } = request.body;;
+    return response.json({ name });
 });
 
 app.listen(3333, () => console.log("Server is running!"));

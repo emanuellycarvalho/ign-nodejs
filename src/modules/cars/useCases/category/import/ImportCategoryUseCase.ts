ea895969dbdd8bba795cfdffec4250406d75d5ea
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-09-17 11:32:44 -0300

    Deleting temporary file after reading with fs

--- src/modules/cars/useCases/category/import/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/category/import/ImportCategoryUseCase.ts
@@ -41,6 +41,7 @@ class ImportCategoryUseCase{
                 });
             })
             .on("end", () => {
+                fs.promises.unlink(file.path);
                 resolve(categories);
             })
             .on("error", (err) => {

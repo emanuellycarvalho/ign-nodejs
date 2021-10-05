42891474897937900e0b2046cc508ece5bedc81c
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2021-10-05 19:18:27 -0300

    fix: resolve docker-compose errors

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,10 +4,10 @@ interface IOptions {
   host: string;
 }
 
-getConnectionOptions().then(options => {
+getConnectionOptions().then(async options => {
   const newOptions = options as IOptions;
   newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
+  await createConnection({
     ...options,
   });
 });
